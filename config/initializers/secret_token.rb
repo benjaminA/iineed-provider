e3c5cb12d903fac9f821b2458b6619bd3dd495e8
unknown <beatriz@beatriz-PC.(none)>
2011-11-30 17:37:03 +0100

    first app iineed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IineedProvider::Application.config.secret_token = '24de6bc9114dd5a2fc1fc977d3964588c29cdb892b4b4ee3430d82143eb75191a65a1ccd3d9594dfd06e4720e9d840de9b9cc3baffd1d6b7cba56a433ea32120'
